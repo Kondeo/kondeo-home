ca8e76c7c6fab683a4df11b733f497a71bb43f36
Julian Poyourow <julianpoyo@gmail.com>
2015-07-11 10:23:21 -0700

    Scaffolded application with yo-angular

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name kondeoHomeApp
+ * @description
+ * # kondeoHomeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('kondeoHomeApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
