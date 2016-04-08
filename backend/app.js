9d2d1e2c64654aa3b7e44e4a34c007b6dcf24227
Julian Poyourow <julianpoyo@gmail.com>
2016-04-07 20:56:47 -0700

    Added CORS to backend

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -14,6 +15,9 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+//Use cors for cross origin support
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
