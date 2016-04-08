6f3d3b2f18e20542e52f42ea97573ef841ba3624
Julian Poyourow <julianpoyo@gmail.com>
2016-04-08 09:27:30 -0700

    Added invoices schema

---
+++ backend/models/invoices.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose'),
+  moment = require('moment');
+var Invoice = new mongoose.Schema({
+    accountId: {
+        type: String,
+        require: 'Please provide the id of the account'
+    },
+    paid: {
+        type: Boolean,
+        default: false
+    },
+    total: {
+        type: Number,
+        require: "Please provide the total amount of the invoice"
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    due: {
+        type: Date,
+        default: moment().add(14, 'd')
+    },
+    documents: [{
+      title: {
+        type: String
+      },
+      description: {
+        type: String
+      },
+      url: {
+        type: String
+      }
+    }]
+});
+
+mongoose.model('Invoice', Invoice);
